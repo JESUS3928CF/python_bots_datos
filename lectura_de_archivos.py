8c31e6e88d0e65cde9f99dddd45f83a203e9a203
Jesús Cf <jesus3928cf@gmail.com>
2024-05-03 22:25:15 -0500

    leer archivos de un csv con pandas

---
+++ lectura_de_archivos.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+#/EXCEL  - CANCER DE MAMA
+#- le vamos a pasar la ubicación del archivo, en este caso como esta en el mismo proyecto no le pasamos la ruta completa 
+cancer_mama = pd.read_csv("breast_cancer.csv") #* Estas funciones reciben una serie de argumentos si nop estas seguro cuales son googlealo
+
+print(cancer_mama)
+
+#/ CSV TITANIC Y VINO ROJO
+
+#/JSON - COVID19
